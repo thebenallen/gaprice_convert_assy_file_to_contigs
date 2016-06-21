787a649ed8ec35da032e89eb88ab4b4081a5660b
Gavin Price <gaprice@lbl.gov>
2016-06-21 11:22:48 -0700

    make
    KBase SDK version 1.0.5 (commit
    f8d5753994d91a0c7e4c07be8aafb77ffaf5bc37)

---
+++ lib/convert_assy_file_to_contigs/convert_assy_file_to_contigsImpl.py
@@ -0,0 +1,40 @@
+#BEGIN_HEADER
+#END_HEADER
+
+
+class convert_assy_file_to_contigs:
+    '''
+    Module Name:
+    convert_assy_file_to_contigs
+
+    Module Description:
+    A KBase module: convert_assy_file_to_contigs
+    '''
+
+    ######## WARNING FOR GEVENT USERS #######
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    #########################################
+    VERSION = "0.0.1"
+    GIT_URL = ""
+    GIT_COMMIT_HASH = "7ef4bad49656c0df54e235ff4ff6c06b55021441"
+    
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
+        #END_CONSTRUCTOR
+        pass
+    
+
+    def status(self, ctx):
+        #BEGIN_STATUS
+        returnVal = {'state': "OK", 'message': "", 'version': self.VERSION, 
+                     'git_url': self.GIT_URL, 'git_commit_hash': self.GIT_COMMIT_HASH}
+        #END_STATUS
+        return [returnVal]
