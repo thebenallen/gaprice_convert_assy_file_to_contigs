7ef4bad49656c0df54e235ff4ff6c06b55021441
Gavin Price <gaprice@lbl.gov>
2016-06-21 11:14:42 -0700

    Initial commit

---
+++ convert_assy_file_to_contigs.spec
@@ -0,0 +1,9 @@
+/*
+A KBase module: convert_assy_file_to_contigs
+*/
+
+module convert_assy_file_to_contigs {
+    /*
+        Insert your typespec information here.
+    */
+};
---
+++ test/convert_assy_file_to_contigs_server_test.py
@@ -0,0 +1,78 @@
+import unittest
+import os
+import json
+import time
+
+from os import environ
+try:
+    from ConfigParser import ConfigParser  # py2
+except:
+    from configparser import ConfigParser  # py3
+
+from pprint import pprint
+
+from biokbase.workspace.client import Workspace as workspaceService
+from convert_assy_file_to_contigs.convert_assy_file_to_contigsImpl import convert_assy_file_to_contigs
+from convert_assy_file_to_contigs.convert_assy_file_to_contigsServer import MethodContext
+
+
+class convert_assy_file_to_contigsTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        token = environ.get('KB_AUTH_TOKEN', None)
+        # WARNING: don't call any logging methods on the context object,
+        # it'll result in a NoneType error
+        cls.ctx = MethodContext(None)
+        cls.ctx.update({'token': token,
+                        'provenance': [
+                            {'service': 'convert_assy_file_to_contigs',
+                             'method': 'please_never_use_it_in_production',
+                             'method_params': []
+                             }],
+                        'authenticated': 1})
+        config_file = environ.get('KB_DEPLOYMENT_CONFIG', None)
+        cls.cfg = {}
+        config = ConfigParser()
+        config.read(config_file)
+        for nameval in config.items('convert_assy_file_to_contigs'):
+            cls.cfg[nameval[0]] = nameval[1]
+        cls.wsURL = cls.cfg['workspace-url']
+        cls.wsClient = workspaceService(cls.wsURL, token=token)
+        cls.serviceImpl = convert_assy_file_to_contigs(cls.cfg)
+
+    @classmethod
+    def tearDownClass(cls):
+        if hasattr(cls, 'wsName'):
+            cls.wsClient.delete_workspace({'workspace': cls.wsName})
+            print('Test workspace was deleted')
+
+    def getWsClient(self):
+        return self.__class__.wsClient
+
+    def getWsName(self):
+        if hasattr(self.__class__, 'wsName'):
+            return self.__class__.wsName
+        suffix = int(time.time() * 1000)
+        wsName = "test_convert_assy_file_to_contigs_" + str(suffix)
+        ret = self.getWsClient().create_workspace({'workspace': wsName})
+        self.__class__.wsName = wsName
+        return wsName
+
+    def getImpl(self):
+        return self.__class__.serviceImpl
+
+    def getContext(self):
+        return self.__class__.ctx
+
+    def test_your_method(self):
+        # Prepare test objects in workspace if needed using 
+        # self.getWsClient().save_objects({'workspace': self.getWsName(), 'objects': []})
+        #
+        # Run your method by
+        # ret = self.getImpl().your_method(self.getContext(), parameters...)
+        #
+        # Check returned data with
+        # self.assertEqual(ret[...], ...) or other unittest methods
+        pass
+        
